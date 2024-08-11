3759e706a08a7e9ffa8e0f29b3526c7bb28aa59c
Saut Sihotang <sautsihotang08@gmail.com>
2024-08-11 22:02:28 +0700

    update

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -121,14 +121,14 @@ watch(
                 <div class="flex justify-content-between mb-3">
                     <div>
                         <span class="block text-500 font-medium mb-3">EOQ</span>
-                        <div class="text-900 font-medium text-xl">152</div>
+                        <!-- <div class="text-900 font-medium text-xl">152</div> -->
                     </div>
                     <div class="flex align-items-center justify-content-center bg-blue-100 border-round" style="width: 2.5rem; height: 2.5rem">
                         <i class="pi pi-fw pi-calculator text-blue-500 text-xl"></i>
                     </div>
                 </div>
-                <span class="text-green-500 font-medium">24 new </span>
-                <span class="text-500">since last visit</span>
+                <!-- <span class="text-green-500 font-medium">24 new </span>
+                <span class="text-500">since last visit</span> -->
             </div>
         </div>
         <div class="col-12 lg:col-6 xl:col-3">
@@ -136,14 +136,14 @@ watch(
                 <div class="flex justify-content-between mb-3">
                     <div>
                         <span class="block text-500 font-medium mb-3">Barang</span>
-                        <div class="text-900 font-medium text-xl">$2.100</div>
+                        <!-- <div class="text-900 font-medium text-xl">$2.100</div> -->
                     </div>
                     <div class="flex align-items-center justify-content-center bg-orange-100 border-round" style="width: 2.5rem; height: 2.5rem">
                         <i class="pi pi-fw pi-th-large text-orange-500 text-xl"></i>
                     </div>
                 </div>
-                <span class="text-green-500 font-medium">%52+ </span>
-                <span class="text-500">since last week</span>
+                <!-- <span class="text-green-500 font-medium">%52+ </span>
+                <span class="text-500">since last week</span> -->
             </div>
         </div>
         <div class="col-12 lg:col-6 xl:col-3">
@@ -151,14 +151,14 @@ watch(
                 <div class="flex justify-content-between mb-3">
                     <div>
                         <span class="block text-500 font-medium mb-3">Pemesanan</span>
-                        <div class="text-900 font-medium text-xl">28441</div>
+                        <!-- <div class="text-900 font-medium text-xl">28441</div> -->
                     </div>
                     <div class="flex align-items-center justify-content-center bg-cyan-100 border-round" style="width: 2.5rem; height: 2.5rem">
                         <i class="pi pi-fw pi-shopping-cart text-cyan-500 text-xl"></i>
                     </div>
                 </div>
-                <span class="text-green-500 font-medium">520 </span>
-                <span class="text-500">newly registered</span>
+                <!-- <span class="text-green-500 font-medium">520 </span>
+                <span class="text-500">newly registered</span> -->
             </div>
         </div>
         <div class="col-12 lg:col-6 xl:col-3">
@@ -166,14 +166,14 @@ watch(
                 <div class="flex justify-content-between mb-3">
                     <div>
                         <span class="block text-500 font-medium mb-3">Penjualan</span>
-                        <div class="text-900 font-medium text-xl">152 Unread</div>
+                        <!-- <div class="text-900 font-medium text-xl">152 Unread</div> -->
                     </div>
                     <div class="flex align-items-center justify-content-center bg-purple-100 border-round" style="width: 2.5rem; height: 2.5rem">
                         <i class="pi pi-fw pi-shopping-bag text-purple-500 text-xl"></i>
                     </div>
                 </div>
-                <span class="text-green-500 font-medium">85 </span>
-                <span class="text-500">responded</span>
+                <!-- <span class="text-green-500 font-medium">85 </span>
+                <span class="text-500">responded</span> -->
             </div>
         </div>
         <div class="col-12 lg:col-6 xl:col-3">
@@ -181,14 +181,14 @@ watch(
                 <div class="flex justify-content-between mb-3">
                     <div>
                         <span class="block text-500 font-medium mb-3">Supplier</span>
-                        <div class="text-900 font-medium text-xl">152 Unread</div>
+                        <!-- <div class="text-900 font-medium text-xl">152 Unread</div> -->
                     </div>
                     <div class="flex align-items-center justify-content-center bg-purple-100 border-round" style="width: 2.5rem; height: 2.5rem">
                         <i class="pi pi-fw pi-truck text-purple-500 text-xl"></i>
                     </div>
                 </div>
-                <span class="text-green-500 font-medium">85 </span>
-                <span class="text-500">responded</span>
+                <!-- <span class="text-green-500 font-medium">85 </span>
+                <span class="text-500">responded</span> -->
             </div>
         </div>
 
@@ -197,14 +197,14 @@ watch(
                 <div class="flex justify-content-between mb-3">
                     <div>
                         <span class="block text-500 font-medium mb-3">Penyimpanan</span>
-                        <div class="text-900 font-medium text-xl">152 Unread</div>
+                        <!-- <div class="text-900 font-medium text-xl">152 Unread</div> -->
                     </div>
                     <div class="flex align-items-center justify-content-center bg-purple-100 border-round" style="width: 2.5rem; height: 2.5rem">
                         <i class="pi pi-fw pi-shield text-purple-500 text-xl"></i>
                     </div>
                 </div>
-                <span class="text-green-500 font-medium">85 </span>
-                <span class="text-500">responded</span>
+                <!-- <span class="text-green-500 font-medium">85 </span>
+                <span class="text-500">responded</span> -->
             </div>
         </div>
 
